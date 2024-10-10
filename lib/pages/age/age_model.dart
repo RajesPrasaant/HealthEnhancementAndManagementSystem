a8bacc82a112bfc0bfd128aa5b8f2b1681670f23
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-10-10 02:48:38 +0000

    newme

---
+++ lib/pages/age/age_model.dart
@@ -0,0 +1,16 @@
+import '/flutter_flow/flutter_flow_util.dart';
+import 'age_widget.dart' show AgeWidget;
+import 'package:flutter/material.dart';
+
+class AgeModel extends FlutterFlowModel<AgeWidget> {
+  ///  State fields for stateful widgets in this page.
+
+  // State field(s) for Slider widget.
+  double? sliderValue;
+
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
